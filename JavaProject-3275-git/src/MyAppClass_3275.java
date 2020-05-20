be681df35b18846cfcd19c2d47c886ed3eaf8bd0
gustavoFreitas <pereiradefreitasg@student.douglascollege.ca>
2020-05-19 23:45:15 -0700

    #3 commit from Lab-02 - Testing the Java Class changes

--- JavaProject-3275-git/src/MyAppClass_3275.java
+++ JavaProject-3275-git/src/MyAppClass_3275.java
@@ -13,7 +13,12 @@ public class MyAppClass_3275 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] studentsID = {300303231,300314567,200204586,400406576,800807654,100102526,300301213,300305254};
+		String userInput;
+			
+		for(int j=0;j<studentsID.length;j++) {
+			System.out.println(studentsID[j]);
+		}	
 	}
 
 }
