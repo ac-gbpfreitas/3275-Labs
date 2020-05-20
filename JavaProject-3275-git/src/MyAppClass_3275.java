f487c96265c810d0be51e57bbdc206fa99233c5b
gustavoFreitas <pereiradefreitasg@student.douglascollege.ca>
2020-05-19 23:40:28 -0700

    #2 commit from Lab-02 - testing /src files changes

---
+++ JavaProject-3275-git/src/MyAppClass_3275.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Owner
+ *
+ */
+public class MyAppClass_3275 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
